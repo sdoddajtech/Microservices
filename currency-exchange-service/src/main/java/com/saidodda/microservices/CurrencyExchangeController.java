846c430155e0811dcf55ff20b7a26f1318709787
Sai Dodda <Sai Dodda@LAPTOP-L98MTH10>
2019-09-13 07:21:03 +0530

    Commited Currency Exchange Service

---
+++ currency-exchange-service/src/main/java/com/saidodda/microservices/CurrencyExchangeController.java
@@ -0,0 +1,30 @@
+package com.saidodda.microservices;
+
+//import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.cloud.config.environment.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CurrencyExchangeController {
+	
+	//@Autowired
+	//private Environment environment;
+	
+	@Autowired
+	private ExchangeValueRepository repository;
+	
+	
+	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	public ExchangeValue retrieveExchangeValue(@PathVariable String from, @PathVariable String to) {
+		//ExchangeValue exchangeValue = new ExchangeValue(100L,from,to,BigDecimal.valueOf(65));
+		ExchangeValue exchangeValue = 
+				repository.findByFromAndTo(from, to);
+		//exchangeValue.setPort(Integer.parseInt(evironment.getProperty("local.server.port")));
+		return exchangeValue;
+	}
+
+}
---
+++ currency-exchange-service/src/main/java/com/saidodda/microservices/ExchangeValueRepository.java
@@ -0,0 +1,11 @@
+package com.saidodda.microservices;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ExchangeValueRepository extends JpaRepository<ExchangeValue, Long> {
+	
+	ExchangeValue findByFromAndTo(String from, String to);
+	
+	
+
+}
